54ef66c323105c78fa8e71577486aca5851b49d5
N9ALV <a@n9.au>
2026-03-01 22:08:41 +0800

    fix: add Astro Netlify adapter for Netlify build

---
+++ astro.config.mjs
@@ -0,0 +1,77 @@
+import { defineConfig, fontProviders } from 'astro/config';
+import db from '@astrojs/db';
+import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
+  build: {
+    inlineStylesheets: 'always'
+  },
+  experimental: {
+    clientPrerender: true,
+    fonts: [
+      {
+        provider: fontProviders.google({
+          experimental: { variableAxis: { Inter: { opsz: ['14..32'] } } }
+        }),
+        name: 'Inter',
+        cssVariable: '--astro-font-inter',
+        weights: ['300 900'],
+        styles: ['normal'],
+        subsets: ['latin']
+      }
+    ]
+  },
+  image: {
+    remotePatterns: [
+      {
+        protocol: 'https'
+      },
+      {
+        protocol: 'http'
+      }
+    ]
+  },
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport'
+  },
+  site: 'https://whiskey.fm',
+  trailingSlash: 'never',
+  integrations: [
+    db(),
+    preact(),
+    sitemap({
+      filter: (page) => {
+        const pathname = new URL(page).pathname;
+        // Exclude episode number pages and only include slug pages.
+        return !/^\/\d+\/?$/.test(pathname);
+      }
+    })
+  ],
+  // These were specific redirects we needed for our podcast, if you do not have any routes to redirect, you can safely remove this.
+  redirects: {
+    '/hot-takes-tan-stack-and-open-source-with-tanner-linsley':
+      '/hot-takes-tanstack-and-open-source-with-tanner-linsley',
+    '/creating-code-pen-tackling-tailwind-and-keeping-it-simple-with-chris-coyier':
+      'creating-codepen-tackling-tailwind-and-keeping-it-simple-with-chris-coyier',
+    '/coding-languages-ai-and-the-evolution-of-game-development-with-phillip-winston':
+      '/coding-languages-ai-and-the-evolution-of-game-development-with-philip-winston'
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    ssr: {
+      external: ['@astrojs/db']
+    },
+    build: {
+      rollupOptions: {
+        external: ['@astrojs/db']
+      }
+    }
+  }
+});
